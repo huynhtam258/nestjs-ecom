8dc54f7a54b48a5e61f140988a05347b2c7b7abe
HuynhManhTam <huynhtam258@gmail.com>
2025-03-14 18:15:11 +0700

    feat: Zod Validation và Serialization

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,4 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ZodSerializerDto } from 'nestjs-zod'
+import { RegisterBodyDTO, RegisterResDTO } from 'src/routes/auth/auth.dto'
+
 
 import { AuthService } from 'src/routes/auth/auth.service'
 
@@ -7,7 +10,8 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: any) {
+  @ZodSerializerDto(RegisterResDTO)
+  async register(@Body() body: RegisterBodyDTO) {
     return await this.authService.register(body)
   }
 

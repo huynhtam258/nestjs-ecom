268229aae837e1b0dec9284fda6a4b7a8a89274d
HuynhManhTam <huynhtam258@gmail.com>
2025-03-23 12:18:53 +0700

    feat: role

---
+++ src/routes/role/role.error.ts
@@ -0,0 +1,9 @@
+
+import { UnprocessableEntityException } from '@nestjs/common'
+
+export const RoleAlreadyExistsException = new UnprocessableEntityException([
+  {
+    message: 'Error.RoleAlreadyExists',
+    path: 'name',
+  },
+])
---
+++ src/routes/role/role.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common'
+import { RoleRepo } from 'src/routes/role/role.repo'
+import { CreateRoleBodyType, GetRolesQueryType, UpdateRoleBodyType } from 'src/routes/role/role.model'
+import { NotFoundRecordException } from 'src/shared/error'
+import { isNotFoundPrismaError, isUniqueConstraintPrismaError } from 'src/shared/helpers'
+import { RoleAlreadyExistsException } from 'src/routes/role/role.error'
+
+@Injectable()
+export class RoleService {
+  constructor(private roleRepo: RoleRepo) {}
+
+  async list(pagination: GetRolesQueryType) {
+    const data = await this.roleRepo.list(pagination)
+    return data
+  }
+
+  async findById(id: number) {
+    const role = await this.roleRepo.findById(id)
+    if (!role) {
+      throw NotFoundRecordException
+    }
+    return role
+  }
+
+  async create({ data, createdById }: { data: CreateRoleBodyType; createdById: number }) {
+    try {
+      const role = await this.roleRepo.create({
+        createdById,
+        data,
+      })
+      return role
+    } catch (error) {
+      if (isUniqueConstraintPrismaError(error)) {
+        throw RoleAlreadyExistsException
+      }
+      throw error
+    }
+  }
+
+  async update({ id, data, updatedById }: { id: number; data: UpdateRoleBodyType; updatedById: number }) {
+    try {
+      const role = await this.roleRepo.update({
+        id,
+        updatedById,
+        data,
+      })
+      return role
+    } catch (error) {
+      if (isNotFoundPrismaError(error)) {
+        throw NotFoundRecordException
+      }
+      if (isUniqueConstraintPrismaError(error)) {
+        throw RoleAlreadyExistsException
+      }
+      throw error
+    }
+  }
+
+  async delete({ id, deletedById }: { id: number; deletedById: number }) {
+    try {
+      await this.roleRepo.delete({
+        id,
+        deletedById,
+      })
+      return {
+        message: 'Delete successfully',
+      }
+    } catch (error) {
+      if (isNotFoundPrismaError(error)) {
+        throw NotFoundRecordException
+      }
+      throw error
+    }
+  }
+}

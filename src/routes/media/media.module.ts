ef6b9e8d2a720392b11dda026c435bae86380679
HuynhManhTam <huynhtam258@gmail.com>
2025-05-08 22:37:41 +0700

    feat: media upload service

---
+++ src/routes/media/media.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { MulterModule } from "@nestjs/platform-express";
+import multer from "multer";
+import path from "path";
+import { generateRandomFilename } from "src/shared/helpers";
+import { MediaController } from "./media.controller";
+const UPLOAD_DIR = path.resolve('upload')
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, UPLOAD_DIR)
+  },
+  filename: function (req, file, cb) {
+    const newFilename = generateRandomFilename(file.originalname)
+    cb(null, newFilename)
+  }
+})
+@Module({
+  imports: [
+    MulterModule.register({
+      storage
+    })
+  ],
+  controllers: [ MediaController ]
+})
+export class MediaModule {}
